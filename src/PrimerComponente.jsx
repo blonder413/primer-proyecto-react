09832103d822e07dbf94f9ec32762c1a700c4736
Jonathan Morales <blonder413@gmail.com>
2023-12-10 11:44:01 -0500

    constantes

--- src/PrimerComponente.jsx
+++ src/PrimerComponente.jsx
@@ -1,5 +1,13 @@
+const string = "esto es un texto"
+const number = 413
+const array = ["reactjs", "javascript"]
+const boolean = true
+const funcion = () => 1+1
+const objeto = {nombre: "reactj", lenguaje: "javascript"}
+const fecha = new Date()
+
 export const PrimerComponente = () => {
     return (
-        <h1>Primer Componente</h1>
+        <p>{ JSON.stringify(objeto) }</p>
     )
 }
