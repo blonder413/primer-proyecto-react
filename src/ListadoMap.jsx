c834628d89e27ced9025c8a184f8d68b007b29d9
Jonathan Morales <blonder413@gmail.com>
2023-12-12 11:48:09 -0500

    map

---
+++ src/ListadoMap.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const Items = ({ nombre, visto }) => {
+    return (
+        // <li>{nombre} {visto ? "✅" : "⛔"}</li>
+        <li>
+            {nombre} {visto && "✅"}
+        </li>
+    );
+};
+
+let i = 6
+
+export const ListadoMap = () => {
+    const agregarTarea = () => {
+        i = i+1;
+        setArreglo([...arreglo, { id: i, nombre: "nuevo", visto: true }]);
+    };
+    let listado = [
+        { id: 1, nombre: "Jill", visto: true },
+        { id: 2, nombre: "Chris", visto: true },
+        { id: 3, nombre: "Barry", visto: true },
+        { id: 4, nombre: "Claire", visto: true },
+        { id: 5, nombre: "Leon", visto: false },
+        { id: 6, nombre: "Rebecca", visto: false },
+    ];
+    const [arreglo, setArreglo] = useState(listado);
+    return (
+        <>
+            <h1>Listado App</h1>
+            <ol>
+                {arreglo.map((item) => (
+                    <Items
+                        key={item.id}
+                        nombre={item.nombre}
+                        visto={item.visto}
+                    ></Items>
+                ))}
+            </ol>
+            <button onClick={() => agregarTarea()}>Agregar tarea</button>
+        </>
+    );
+};
--- src/main_listado_app.jsx
+++ src/main_listado_app.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './estilos.css'
 import { ListadoApp } from './ListadoApp'
+import { ListadoMap } from './ListadoMap'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ListadoApp></ListadoApp>
+    {/* <ListadoApp></ListadoApp> */}
+    <ListadoMap></ListadoMap>
   </React.StrictMode>,
 )
